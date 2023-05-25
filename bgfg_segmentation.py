9457efe8816907c9ff33270f20ced9217be4842e
Yusfia Hafid Aristyagama <yusfia.hafid@staff.uns.ac.id>
2023-05-25 13:35:09 +0700

    absolute difference

--- bgfg_segmentation.py
+++ bgfg_segmentation.py
@@ -1,6 +1,8 @@
 import cv2 as cv
+import numpy as np
 
 capture = cv.VideoCapture('data/mouse.mp4')
+#capture = cv.VideoCapture(0)
 if not capture.isOpened():
     exit(0)
 # subsMog2 = cv.createBackgroundSubtractorMOG2(600,125,False)
@@ -14,16 +16,40 @@ while (capture.isOpened()):
     height = int(frame.shape[0] * scale / 100)
     dim = (width, height)
     image = cv.resize(frame,dim, cv.INTER_AREA)
-
+    gaussian = np.array([
+        [1.0, 4.0, 7.0, 4.0, 1.0],
+        [4.0, 16.0, 26.0, 16.0, 4.0],
+        [7.0, 26.0, 41.0, 26.0, 7.0],
+        [4.0, 16.0, 26.0, 16.0, 4.0],
+        [1.0, 4.0, 7.0, 4.0, 1.0]
+    ])/273
+    image = cv.filter2D(image,-1,gaussian)
     if (i==0):
         frame1 = image
         grayscaleframe1 = cv.cvtColor(frame1, cv.COLOR_RGBA2GRAY)
+        i = i+1
 
+    #absolute difference
+    grayscaleframe = cv.cvtColor(image, cv.COLOR_RGBA2GRAY)
+    framedelta = cv.absdiff(grayscaleframe1,grayscaleframe)
+    _, bgs = cv.threshold(framedelta, 50, 255, cv.THRESH_BINARY)
+
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (7,7))
+    morphImage = bgs
+    for i in range(1,3):
+        morphImage = cv.morphologyEx(morphImage, cv.MORPH_CLOSE, kernel)
+    morphImage = cv.morphologyEx(morphImage, cv.MORPH_DILATE, kernel)
+    #MOG2
     blobMog = subsMog2.apply(image)
+
+    #KNN
     blobKNN = subsKNN.apply(image)
+
     cv.imshow("image asli",image)
     cv.imshow("image mog",blobMog)
     cv.imshow("image knn",blobKNN)
+    cv.imshow("image abs diff", bgs)
+    cv.imshow("morph image", morphImage)
     keyword = cv.waitKey(30)
     if keyword=='q' or keyword==27:
         break
